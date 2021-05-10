43c2c37e83685ec9c48665f1ca1b10e84db74a67
Rajiv Sharma <Rajiv.Sharma@anz.com>
2021-05-10 18:28:20 +0530

    Test Destroy

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "google_compute_instance" "vm_instance" {
-  name         = "terraform-instance"
+  name         = "demo-instance"
   machine_type = "f1-micro"
 
   boot_disk {
@@ -9,7 +9,7 @@ resource "google_compute_instance" "vm_instance" {
   }
 
   network_interface {
-    network = "default"
+    subnetwork = "default"
   }
 }
 
